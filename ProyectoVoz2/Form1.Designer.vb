c05fbde4f76093cbcfc567fe81df995c0aefa7e6
Armando Bolaños <45885565+ArmandoBolanios@users.noreply.github.com>
2019-08-31 19:37:27 -0500

    Aplicación de reconocimiento de voz
    Realizado en Visual Basic para detectar voz.

---
+++ ProyectoVoz2/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txt_TextoRec = New System.Windows.Forms.TextBox()
+        Me.btnDeterminar = New System.Windows.Forms.Button()
+        Me.btnConectar = New System.Windows.Forms.Button()
+        Me.btnEnviar = New System.Windows.Forms.Button()
+        Me.cmbPuertos = New System.Windows.Forms.ComboBox()
+        Me.txtEnviar = New System.Windows.Forms.TextBox()
+        Me.txtRecibe = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.USBConex = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'txt_TextoRec
+        '
+        Me.txt_TextoRec.Location = New System.Drawing.Point(392, 28)
+        Me.txt_TextoRec.Name = "txt_TextoRec"
+        Me.txt_TextoRec.Size = New System.Drawing.Size(100, 20)
+        Me.txt_TextoRec.TabIndex = 0
+        '
+        'btnDeterminar
+        '
+        Me.btnDeterminar.Location = New System.Drawing.Point(22, 12)
+        Me.btnDeterminar.Name = "btnDeterminar"
+        Me.btnDeterminar.Size = New System.Drawing.Size(135, 23)
+        Me.btnDeterminar.TabIndex = 1
+        Me.btnDeterminar.Text = "Determinar conexión"
+        Me.btnDeterminar.UseVisualStyleBackColor = True
+        '
+        'btnConectar
+        '
+        Me.btnConectar.Location = New System.Drawing.Point(239, 88)
+        Me.btnConectar.Name = "btnConectar"
+        Me.btnConectar.Size = New System.Drawing.Size(75, 23)
+        Me.btnConectar.TabIndex = 2
+        Me.btnConectar.Text = "Conectar"
+        Me.btnConectar.UseVisualStyleBackColor = True
+        '
+        'btnEnviar
+        '
+        Me.btnEnviar.Location = New System.Drawing.Point(239, 163)
+        Me.btnEnviar.Name = "btnEnviar"
+        Me.btnEnviar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnviar.TabIndex = 3
+        Me.btnEnviar.Text = "Enviar"
+        Me.btnEnviar.UseVisualStyleBackColor = True
+        '
+        'cmbPuertos
+        '
+        Me.cmbPuertos.FormattingEnabled = True
+        Me.cmbPuertos.Location = New System.Drawing.Point(22, 90)
+        Me.cmbPuertos.Name = "cmbPuertos"
+        Me.cmbPuertos.Size = New System.Drawing.Size(164, 21)
+        Me.cmbPuertos.TabIndex = 4
+        '
+        'txtEnviar
+        '
+        Me.txtEnviar.Location = New System.Drawing.Point(22, 163)
+        Me.txtEnviar.Name = "txtEnviar"
+        Me.txtEnviar.Size = New System.Drawing.Size(193, 20)
+        Me.txtEnviar.TabIndex = 5
+        '
+        'txtRecibe
+        '
+        Me.txtRecibe.Location = New System.Drawing.Point(22, 234)
+        Me.txtRecibe.Multiline = True
+        Me.txtRecibe.Name = "txtRecibe"
+        Me.txtRecibe.Size = New System.Drawing.Size(193, 105)
+        Me.txtRecibe.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Lista de puertos"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 137)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Datos a enviar"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(19, 209)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Datos recibidos"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(389, 12)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Reconocer voz"
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(513, 426)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtRecibe)
+        Me.Controls.Add(Me.txtEnviar)
+        Me.Controls.Add(Me.cmbPuertos)
+        Me.Controls.Add(Me.btnEnviar)
+        Me.Controls.Add(Me.btnConectar)
+        Me.Controls.Add(Me.btnDeterminar)
+        Me.Controls.Add(Me.txt_TextoRec)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txt_TextoRec As System.Windows.Forms.TextBox
+    Friend WithEvents btnDeterminar As System.Windows.Forms.Button
+    Friend WithEvents btnConectar As System.Windows.Forms.Button
+    Friend WithEvents btnEnviar As System.Windows.Forms.Button
+    Friend WithEvents cmbPuertos As System.Windows.Forms.ComboBox
+    Friend WithEvents txtEnviar As System.Windows.Forms.TextBox
+    Friend WithEvents txtRecibe As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents USBConex As System.IO.Ports.SerialPort
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ ProyectoVoz2/Form1.vb
@@ -0,0 +1,146 @@
+﻿Imports System.Speech.Recognition
+
+Public Class Form1
+
+    Dim GRABA As New SpeechRecognitionEngine
+    Dim PALABRA As String
+
+    Dim cadenaEntrada As String
+    Dim cadenaSalida As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim VOCABULARIO As New GrammarBuilder
+        VOCABULARIO.Append(New Choices("encender cocina", "encender cuarto", "encender sala", "encender baño", "encender todo",
+                                       "apagar cocina", "apagar cuarto", "apagar sala", "apagar baño", "apagar todo"))
+        GRABA.LoadGrammar(New Grammar(VOCABULARIO))
+
+        GRABA.SetInputToDefaultAudioDevice()
+        GRABA.RecognizeAsync(RecognizeMode.Multiple)
+        AddHandler GRABA.SpeechRecognized, AddressOf RECONOCE
+
+        cadenaEntrada = ""
+        cadenaSalida = ""
+        btnConectar.Enabled = False
+        btnEnviar.Enabled = False
+        Timer1.Enabled = False
+
+    End Sub
+
+    Public Sub RECONOCE(ByVal sender As Object, ByVal e As SpeechRecognizedEventArgs)
+        Dim RESULTADO As RecognitionResult
+        RESULTADO = e.Result
+        PALABRA = RESULTADO.Text
+        txt_TextoRec.Text = RESULTADO.Text
+
+        If btnEnviar.Enabled = True Then
+            If PALABRA = "encender cocina" Then
+                cadenaSalida = "a"
+                USBConex.Write(cadenaSalida)
+            End If
+            If PALABRA = "encender cuarto" Then
+                cadenaSalida = "e"
+                USBConex.Write(cadenaSalida)
+            End If
+            If PALABRA = "encender sala" Then
+                cadenaSalida = "i"
+                USBConex.Write(cadenaSalida)
+            End If
+            If PALABRA = "encender baño" Then
+                cadenaSalida = "o"
+                USBConex.Write(cadenaSalida)
+            End If
+
+            If PALABRA = "apagar cocina" Then
+                cadenaSalida = "A "
+                USBConex.Write(cadenaSalida)
+            End If
+            If PALABRA = "apagar cuarto" Then
+                cadenaSalida = "E"
+                USBConex.Write(cadenaSalida)
+            End If
+            If PALABRA = "apagar sala" Then
+                cadenaSalida = "I"
+                USBConex.Write(cadenaSalida)
+            End If
+            If PALABRA = "apagar baño" Then
+                cadenaSalida = "O"
+                USBConex.Write(cadenaSalida)
+            End If
+
+            If PALABRA = "encender todo" Then
+                cadenaSalida = "X"
+                USBConex.Write(cadenaSalida)
+            End If
+            If PALABRA = "apagar todo" Then
+                cadenaSalida = "T"
+                USBConex.Write(cadenaSalida)
+            End If
+
+        End If
+
+    End Sub
+
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub btnDeterminar_Click(sender As Object, e As EventArgs) Handles btnDeterminar.Click
+        cmbPuertos.Items.Clear()
+        For Each puertoDisponible As String In My.Computer.Ports.SerialPortNames 'se almacenarán en esta variable >puertoDisponible
+            cmbPuertos.Items.Add(puertoDisponible)
+        Next
+
+        If cmbPuertos.Items.Count > 0 Then
+            cmbPuertos.Text = cmbPuertos.Items(0) 'va a encontrar el primer elemento que es 0
+            MessageBox.Show("Seleccione el puerto a utilizar")
+            btnConectar.Enabled = True
+        Else
+            MessageBox.Show("Ningun puerto encontrado :( ")
+            btnConectar.Enabled = False
+            btnEnviar.Enabled = False
+            cmbPuertos.Items.Clear()
+            cmbPuertos.Text = ""
+
+        End If
+    End Sub
+
+    Private Sub btnConectar_Click(sender As Object, e As EventArgs) Handles btnConectar.Click
+        If btnConectar.Text = "Conectar" Then
+            USBConex.PortName = cmbPuertos.Text
+            btnConectar.Text = "Desconectar"
+            btnEnviar.Enabled = True
+            Timer1.Enabled = True
+            USBConex.Open()
+        Else
+            btnConectar.Text = "Conectar"
+            btnEnviar.Enabled = False
+            Timer1.Enabled = False
+            USBConex.Close()
+
+        End If
+    End Sub
+
+    Sub enviar()
+        USBConex.DiscardInBuffer()
+        cadenaSalida = txt_TextoRec.Text()
+        USBConex.Write(cadenaSalida)
+    End Sub
+
+    Private Sub btnEnviar_Click(sender As Object, e As EventArgs) Handles btnEnviar.Click
+        enviar()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        cadenaEntrada = USBConex.ReadExisting
+        If cadenaEntrada <> "" Then
+            txtRecibe.Text = txtRecibe.Text + cadenaEntrada
+            cadenaEntrada = ""
+            USBConex.DiscardInBuffer()
+        End If
+
+    End Sub
+
+    Private Sub txt_TextoRec_TextChanged(sender As Object, e As EventArgs) Handles txt_TextoRec.TextChanged
+    End Sub
+End Class
